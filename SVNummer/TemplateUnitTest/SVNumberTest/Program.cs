72d0b8c1319960c1af38dc073caa5a81ce8e683e
ImNotIsaac03IV <isaac.darko04@outlook.com>
2025-09-18 10:03:38 +0200

    Add SVNummer

---
+++ SVNummer/TemplateUnitTest/SVNumberTest/Program.cs
@@ -0,0 +1,44 @@
+﻿/*--------------------------------------------------------------
+ *				HTBLA-Leonding / Class: 1xHIF
+ *--------------------------------------------------------------
+ *              Musterlösung-HA
+ *--------------------------------------------------------------
+ * Description: Test a SV Number
+ *--------------------------------------------------------------
+ */
+
+using System;
+
+namespace SVNummerTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Check a SV Number");
+            Console.WriteLine("*********************");
+
+            Console.Write("Please enter a SV Number: ");
+            string input = Console.ReadLine();
+
+            while (!ValidateInput(input))
+            {
+                if (SVNummer.IsSvNumberValid(input))
+                {
+                    Console.WriteLine($"The SV Number \"{input}\" is valid");
+                }
+                else
+                {
+                    Console.WriteLine($"The SV Number \"{input}\" is invalid");
+                }
+
+                Console.Write("Please enter a SV Number: ");
+                input = Console.ReadLine();
+            }
+        }
+        static bool ValidateInput(string input)
+        {
+            return String.IsNullOrEmpty(input);
+        }
+    }
+}
---
+++ SVNummer/TemplateUnitTest/SVNumberTest/SVNummer.cs
@@ -0,0 +1,72 @@
+﻿/*--------------------------------------------------------------
+ *				HTBLA-Leonding / Class: 1xHIF
+ *--------------------------------------------------------------
+ *              Musterlösung-HA
+ *--------------------------------------------------------------
+ * Description: Test a SV Number
+ *--------------------------------------------------------------
+ */
+
+namespace SVNummerTest
+{
+    public class SVNummer
+    {
+        public static bool IsSvNumberValid(string svNumber)
+        {
+            svNumber.Trim(' ');
+            int[] svCheck = { 3, 7, 9, 0, 5, 8, 4, 2, 1, 6 };
+
+            if (!isLength10(svNumber))
+            {
+                return false;
+            }
+
+            int validDigit = ValidSum(svCheck, svNumber);
+
+            if (!IsOnlyDigits(svNumber))
+            {
+                return false;
+            }
+
+            if (validDigit != (svNumber[3] - '0'))
+            {
+                return false;
+            }
+
+            return true;
+        }
+        private static bool isLength10 (string input)
+        {
+            if(input.Length == 10)
+            {
+                return true;
+            }
+
+            return false;
+        }
+        private static bool IsOnlyDigits(string input)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] < '0' || input[i] > '9')
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        private static int ValidSum(int[] svCheck, string svNumber)
+        {
+            if (svNumber == "")
+            {
+                return -1;
+            }
+            int sum = 0;
+            for(int i = 0; i < svNumber.Length; i++)
+            {
+                sum += (svNumber[i] - '0') * svCheck[i];
+            }
+            return sum % 11;
+        }
+    }
+}
