15b0580e5bf666e3da770f84b846e501404da2ca
ImNotIsaac03IV <isaac.darko04@outlook.com>
2025-09-18 10:11:58 +0200

    Add Distinct

---
+++ Distinct/Template/Distinct/DistinctTools.cs
@@ -0,0 +1,144 @@
+﻿/*--------------------------------------------------------------
+*				HTBLA-Leonding / Class: 3ABIF
+*--------------------------------------------------------------
+*              Musterlösung-HA
+*--------------------------------------------------------------
+* Description: (Array-)Distinct with UnitTests
+*--------------------------------------------------------------
+*/
+
+using System;
+
+namespace Distinct
+{
+    public static class DistinctTools
+    {
+        public static bool IsDistinct(int[] ar)
+        {
+            for (int i = 0; i < ar.Length; i++)
+            {
+                for (int j = 1 + i; j < ar.Length; j++)
+                {
+                    if (ar[i] == ar[j])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        } //Checks if an array only consists on distinct characters
+        public static int[] Distinct(int[] ar)
+        {
+            int index = 0;
+            int size = ar.Length;
+            int[] modifiedArray = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                bool isDistinct = true;
+
+                for (int j = 0; j < index && isDistinct; j++)
+                {
+                    if (ar[i] == ar[j])
+                    {
+                        isDistinct = false;
+                    }
+                }
+                if (isDistinct)
+                {
+                    modifiedArray[index] = ar[i];
+                    index++;
+                }
+            }
+
+            int[] result = ConcateZeros(modifiedArray, index);
+
+            return result;
+        } //Every value is displayed once
+        public static int[] Duplicate(int[] ar)
+        {
+            if (ar.Length == 0)
+            {
+                return ar;
+            }
+
+            int index = 0;
+            int size = ar.Length;
+            int[] modifiedArray = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                bool isSame = false;
+
+                for (int j = 0; j < i; j++)
+                {
+                    if (ar[i] == ar[j])
+                    {
+                        isSame = true;
+                    }
+                }
+
+                if (isSame)
+                {
+                    bool alreadyAdded = false;
+                    for (int n = 0; n < index; n++)
+                    {
+                        if (modifiedArray[n] == ar[i])
+                        {
+                            alreadyAdded = true;
+                        }
+                    }
+                    if (!alreadyAdded)
+                    {
+                        modifiedArray[index] = ar[i];
+                        index++;
+                    }
+                }
+            }
+            int[] result = ConcateZeros(modifiedArray, index);
+
+            return result;
+        } //This method only displays values that appear more than once
+        public static void Spaces(string label)
+        {
+            int space = 10;
+            for (int i = label.Length; i < space; i++)
+            {
+                Console.Write(" ");
+            }
+        } //This method is for formatting the output
+        public static int[] InsertValues(int size)
+        {
+            int[] array = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write($"Please enter {i + 1}. numbers: ");
+                int value = Convert.ToInt32(Console.ReadLine());
+                array[i] = value;
+            }
+            return array;
+        } // This method is for entering the values of my array
+        public static void ArrayOutput(int[] array)
+        {
+            int counter = 0;
+            foreach (int i in array)
+            {
+                if (counter > 0)
+                {
+                    Console.Write(", ");
+                }
+                Console.Write(i);
+                counter++;
+            }
+        } //A Simple method for printing the values of an array to the console
+        public static int[] ConcateZeros(int[] array, int index)
+        {
+            int[] outputArray = new int[index];
+            for (int i = 0; i < index; i++)
+            {
+                outputArray[i] = array[i];
+            }
+            return outputArray;
+        } //Removes zeros
+    }
+}
---
+++ Distinct/Template/Distinct/Program.cs
@@ -0,0 +1,54 @@
+﻿/*--------------------------------------------------------------
+*				HTBLA-Leonding / Class: 1xHIF
+*--------------------------------------------------------------
+*              Musterlösung-HA
+*--------------------------------------------------------------
+* Description: (Array-)Distinct with UnitTests
+*--------------------------------------------------------------
+*/
+
+namespace Distinct
+{
+    using System;
+    using System.Diagnostics.Metrics;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Distinct");
+            Console.WriteLine("**********************");
+            Console.Write("Please enter the count of numbers: ");
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            int[] array = new int[size];
+
+            array = DistinctTools.InsertValues(size);
+
+            Console.Write("Input: ");
+            DistinctTools.Spaces("Input");
+            DistinctTools.ArrayOutput(array);
+
+            if (!DistinctTools.IsDistinct(array))
+            {
+                Console.Write(" => The array is NOT distinct");
+            }
+
+            Console.WriteLine();
+
+            Console.Write($"Distinct: ");
+            DistinctTools.Spaces("Distinct");
+            DistinctTools.ArrayOutput(DistinctTools.Distinct(array));
+
+            Console.WriteLine();
+
+            Console.Write("Duplicates: ");
+            DistinctTools.Spaces("Duplicates");
+            DistinctTools.ArrayOutput(DistinctTools.Duplicate(array));
+
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+}
